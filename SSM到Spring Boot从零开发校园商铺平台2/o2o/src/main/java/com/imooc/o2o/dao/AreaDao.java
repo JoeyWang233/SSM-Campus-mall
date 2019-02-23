daceae39f5cd5d7c021a469c3994790e1d0a246e
Joey Wang <joeywang233@gmail.com>
2019-02-23 14:03:54 +0800

    [add]

---
+++ SSM到Spring Boot从零开发校园商铺平台2/o2o/src/main/java/com/imooc/o2o/dao/AreaDao.java
@@ -0,0 +1,17 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+
+    /** 
+    * @Description: 列出区域列表 
+    * @Param: [] 
+    * @return: java.util.List<com.imooc.o2o.entity.Area> 
+    * @Author: Joey
+    * @Date: 2019/2/22 
+    */ 
+    List<Area> queryArea();
+}
---
+++ SSM到Spring Boot从零开发校园商铺平台2/o2o/src/main/java/com/imooc/o2o/service/impl/AreaServiceImpl.java
@@ -0,0 +1,27 @@
+package com.imooc.o2o.service.impl;
+
+import com.imooc.o2o.dao.AreaDao;
+import com.imooc.o2o.entity.Area;
+import com.imooc.o2o.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @program: oto
+ * @description:
+ * @author: Joey
+ * @create: 2019-02-22 20:46
+ */
+@Service
+public class AreaServiceImpl implements AreaService {
+
+    @Autowired
+    private AreaDao areaDao;
+
+    @Override
+    public List<Area> getAreaList() {
+        return areaDao.queryArea();
+    }
+}
