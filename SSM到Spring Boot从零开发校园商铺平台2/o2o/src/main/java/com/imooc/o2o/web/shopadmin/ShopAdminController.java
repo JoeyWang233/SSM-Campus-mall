7644e2772a82146f92365303719468f932ce6975
Joey Wang <joeywang233@gmail.com>
2019-02-25 21:52:37 +0800

    [add]4-9

---
+++ SSM到Spring Boot从零开发校园商铺平台2/o2o/src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
@@ -0,0 +1,21 @@
+package com.imooc.o2o.web.shopadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @program: o2o
+ * @description:
+ * @author: Joey
+ * @create: 2019-02-25 21:43
+ */
+@Controller
+@RequestMapping(value = "/shopadmin", method = {RequestMethod.GET})
+public class ShopAdminController {
+    @RequestMapping(value = "/shopoperation")
+    public String shopOperation() {
+        // 之所以不是"/WEB-INF/html/shop/shopoperation.html"是因为在spring-web.xml文件中定义了viewResolver Bean，其内设置了前后缀
+        return "shop/shopoperation";
+    }
+}
