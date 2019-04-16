0e70b67c1327a4fdeb3e2f2c47208c26432374c5
Joey Wang <joeywang233@gmail.com>
2019-04-16 16:00:20 +0800

    [add]9-3

---
+++ SSM到Spring Boot从零开发校园商铺平台2/o2o/src/main/java/com/imooc/o2o/web/frontend/FrontendController.java
@@ -0,0 +1,21 @@
+package com.imooc.o2o.web.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @program: o2o
+ * @description:
+ * @author: Joey
+ * @create: 2019-04-16 14:18
+ */
+@Controller
+@RequestMapping(value = "/frontend")
+public class FrontendController {
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    private String index(){
+        return "frontend/index";
+    }
+}
